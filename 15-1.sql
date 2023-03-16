f1914e0e3b60757b4a829dcd75371b2fab3cbb85
yeneua <rladPsk1117@gmail.com>
2023-03-16 22:00:25 +0900

    15-3절까지
    발표준비하면서 수정 및 추가

--- 15-1.sql
+++ 15-1.sql
@@ -22,7 +22,6 @@ activity_log_with_landing_exit as (
 select * from activity_log_with_landing_exit;
 
 
-
 -- 각 세션의 입구 페이지와 출구 페이지를 기반으로 방문 횟수를 추출하는 쿼리
 with
 activity_log_with_landing_exit as (
@@ -56,18 +55,18 @@ select 'exit' as type, * from exit_count;
 -- 어떤 페이지에서 조회를 하기 시작해서 어떤 페이지에서 이탈하는지
 with
 activity_log_with_landing_exit as (
-    select session,
+     select session,
            path,
            stamp,
-           first_value(path)
-            over(partition by session
-                order by stamp asc
-                    rows between unbounded preceding and unbounded following
+           first_value(path) -- path 중 가장 첫번째
+            over(partition by session -- session 기준
+                order by stamp asc -- 오름차순(시간순)
+                    rows between unbounded preceding and unbounded following -- 모든 행 대상으로
             ) as landing,
-           last_value(path)
-            over(partition by session
-                order by stamp access_count
-                    rows between unbounded preceding and unbounded following
+           last_value(path) -- path 중 가장 마지막 레코드
+            over(partition by session -- session 기준
+                order by stamp asc -- 오름차순(시간순)
+                    rows between unbounded preceding and unbounded following -- 모든 행 대상으로
             ) as exit
     from activity_log
 )
--- 15-2.sql
+++ 15-2.sql
@@ -20,12 +20,12 @@ with
 activity_log_with_landing_bounce_flag as (
     select *,
            case when row_number() over(partition by session order by stamp asc) = 1 then 1 else 0 end as is_landing, -- 입구 페이지 판정
-           case when count(1) over (partition by session) = 1 then 1 else 0 end as is_bounce -- 직귀 판정 : sessino이 하나인 경우에 1(=첫번째 페이지만 보고 다른 페이지로 이동하지 않았다는 뜻)
+           case when count(1) over (partition by session) = 1 then 1 else 0 end as is_bounce -- 직귀 판정 : session이 하나인 경우에 1(=첫번째 페이지만 보고 다른 페이지로 이동하지 않았다는 뜻)
     from activity_log
 )
 select path,
-       sum(is_bounce) as bounce_count,
-       sum(is_landing) as landing_count,
-       avg(100*case when is_landing = 1 then is_bounce end) as bounce_ratio
+       sum(is_bounce) as bounce_count, -- 직귀 수
+       sum(is_landing) as landing_count, -- 입구 수
+       avg(100*case when is_landing = 1 then is_bounce end) as bounce_ratio -- 직귀율
 from activity_log_with_landing_bounce_flag
 group by path;
