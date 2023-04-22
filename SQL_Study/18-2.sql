b14e69b74328e154619a887079aafb6256f0b606
yeneua <rladPsk1117@gmail.com>
2023-04-22 23:36:45 +0900

    폴더 이동

---
+++ SQL_Study/18-2.sql
@@ -0,0 +1,61 @@
+-- 18-2. 크롤러 제외하기
+
+-- 규칙을 기반으로 크롤러를 제외하는 쿼리
+select *
+from action_log_with_noise
+where not
+    (user_agent LIKE '%bot%'
+    or user_agent LIKE '%crawler%'
+    or user_agent LIKE '%spider%'
+    or user_agent LIKE '%archiver');
+
+
+-- 마스터 데이터를 사용해 제외하기
+with
+mst_bot_user_agent as (
+              select '%bot%' as rule
+    union all select '%crawler%' as rule
+    union all select '%spider%' as rule
+    union all select '%archiver%' as rule
+),
+filtered_action_log as (
+    select
+        l.stamp, l.session, l.action, l.products, l.url, l.ip, l.user_agent
+    from action_log_with_noise as l
+    where
+        not exists(
+                select 1
+                from mst_bot_user_agent as m
+                where l.user_agent LIKE m.rule -- 규칙이 존재하는지 비교
+    )
+)
+select * from filtered_action_log;
+
+
+-- 접근이 많은 사용자 에이전트를 확인하는 쿼리
+with
+mst_bot_user_agent as (
+              select '%bot%' as rule
+    union all select '%crawler%' as rule
+    union all select '%spider%' as rule
+    union all select '%archiver%' as rule
+),
+filtered_action_log as (
+    select
+        l.stamp, l.session, l.action, l.products, l.url, l.ip, l.user_agent
+    from action_log_with_noise as l
+    where
+        not exists(
+                select 1
+                from mst_bot_user_agent as m
+                where l.user_agent LIKE m.rule
+    )
+)
+select
+    user_agent,
+    count(1) as count,
+    100.0*sum(count(1)) over(order by count(1) desc
+    rows between unbounded preceding and current row)/sum(count(1)) over() as cumulative_ratio
+from filtered_action_log
+group by user_agent
+order by count desc;
