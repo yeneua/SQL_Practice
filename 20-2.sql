b85f08bca13d36c22fab1b97b0c8779aa503e921
yeneua <rladPsk1117@gmail.com>
2023-04-22 23:32:16 +0900

    코드 20-7

--- 20-2.sql
+++ 20-2.sql
@@ -65,3 +65,45 @@ from path_ranking as r1
 select *
 from pair_ranking
 order by type1, type2, rank1;
+
+
+-- 스피어만 상관계수로 순위의 유사도를 계산하는 쿼리
+with
+path_stat as (
+select path,
+	   count(distinct long_session) as access_users,
+	   count(distinct short_session) as access_count,
+	   count(*) as page_view
+from access_log
+group by path
+)
+, path_ranking as (
+select 'access_user' as type, path,
+		rank() over(order by access_users desc) as rank
+from path_stat
+union all
+select 'access_count' as type, path,
+		rank() over(order by access_count desc) as rank
+from path_stat
+union all
+select 'page_view' as type, path,
+		rank() over(order by page_view desc) as rank
+from path_stat
+)
+, pair_ranking as(
+select r1.path,
+	   r1.type as type1,
+	   r1.rank as rank1,
+       r2.type as type2,
+	   r2.rank as rank2,
+	   power(r1.rank-r2.rank,2) as diff
+from path_ranking as r1
+	join path_ranking as r2
+	on r1.path = r2.path
+)
+
+select type1,type2,
+	   1-(6.0*sum(diff)/(power(count(1),3)-count(1))) as spearman --  스피어만 상관계수 : 완전일치(1) ~ 완전불일치(-1)
+from pair_ranking
+group by type1,type2
+order by type1,spearman desc;
