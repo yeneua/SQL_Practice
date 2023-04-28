1e70b3eb37ea631b30758ac10e49dc6deb1fd7fa
yeneua <rladPsk1117@gmail.com>
2023-04-28 23:58:32 +0900

    코드 21-15

--- 21-6.sql
+++ 21-6.sql
@@ -29,12 +29,58 @@ search_result_with_correct_items as (
 )
 , search_result_with_recall as (
 	select *,
-		   sum(correct) over(partition by keyword order by coalesce(rank, 10000) asc rows between unbounded preceding and current row) as cum_correct,
-		   case when rank is null then 0.0
-		   else 100.0 * sum(correct) over(partition by keyword order by coalesce(rank, 10000) asc rows between unbounded preceding and current row) / sum(correct) over(partition by keyword)
-		   end as recall
+		   -- 검색 결과 상위에서 정답 데이터에 포함되는 아이템 수의 누계 구하기
+		   sum(correct)
+		   	over(partition by keyword order by coalesce(rank, 10000) asc -- rank가 NULL이면 정렬 순서 마지막위치 -> 편의상 큰 값으로 변환
+			 rows between unbounded preceding and current row) as cum_correct, -- 이전행 전부 ~ 현재 행
+		   case when rank is null then 0.0 -- 검색 결과에 포함되지 않은 아이템(rank가 NULL)은 편의상 0으로 다루기
+		    else
+			 100.0 * sum(correct)
+			  over(partition by keyword order by coalesce(rank, 10000) asc
+			   rows between unbounded preceding and current row) -- 이전 행 전부 ~ 현재 행
+			 / sum(correct) over(partition by keyword) -- keyword 기준으로 correct 개수 
+		   end as recalls
+
 	from search_result_with_correct_items
 )
 select *
 from search_result_with_recall
 order by keyword, rank;
+
+
+-- 검색 결과 상위 5개의 재현율을 키워드별로 추출하는 쿼리
+with
+search_result_with_correct_items as (
+	select coalesce(r.keyword, c.keyword) as keyword,
+		   r.rank,
+	  	   coalesce(r.item, c.item) as item,
+		   case when c.item is not null then 1 else 0 end as correct
+	from search_result as r
+		full outer join correct_result as c
+		on r.keyword = c.keyword and r.item = c.item 
+)
+, search_result_with_recall as (
+	select *,
+		   sum(correct)
+		   	over(partition by keyword order by coalesce(rank, 10000) asc
+			 rows between unbounded preceding and current row) as cum_correct,
+		   case when rank is null then 0.0
+		    else
+			 100.0 * sum(correct)
+			  over(partition by keyword order by coalesce(rank, 10000) asc
+			   rows between unbounded preceding and current row)
+			 / sum(correct) over(partition by keyword) 
+		   end as recall
+	from search_result_with_correct_items
+)
+,
+recall_over_rank_5 as (
+	select keyword, rank, recall,
+		   row_number()  -- 검색 결과 순위 높은 순으로 번호 붙이기
+		    over(partition by keyword order by coalesce(rank,0) desc) as desc_number, -- 검색 결과 나오지 않는 아이템은 편의상 0 부여 
+	from search_result_with_recall
+	where coalesce(rank, 0) <= 5 -- 검색결과 상위 5개 이하 또는 검색결과에 포함되지 않은 아이템만 출력
+)
+select keyword, recall as recall_at_5
+from recall_over_rank_5
+where desc_number = 1; -- 검색 결과 상위 5개 중에서 가장 순위 높은 레코드 추출
